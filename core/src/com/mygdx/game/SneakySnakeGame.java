342d818f062ae10e82ce2830b3d6806e45a83bb9
Kibalo Jules Tinaka <julestnk.dev@gmail.com>
2023-02-25 14:27:30 +0100

    moved screen responsibility to GameScreen class

--- core/src/com/mygdx/game/SneakySnakeGame.java
+++ core/src/com/mygdx/game/SneakySnakeGame.java
@@ -1,31 +1,14 @@
 package com.mygdx.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
 
-public class SneakySnakeGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+import com.badlogic.gdx.Game;
+
+public class SneakySnakeGame extends Game {
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		setScreen(new GameScreen());
 	}
 
-	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+
 }
