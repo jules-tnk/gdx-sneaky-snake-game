bfa8b3f01f6ea2a66f75e51a7b670dc3f27a79d6
Kibalo Jules Tinaka <julestnk.dev@gmail.com>
2023-02-25 15:23:42 +0100

    Add snakehead, snakebody, and apple png assets Make the snake move to right at start

--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -2,27 +2,43 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class GameScreen extends ScreenAdapter {
     private SpriteBatch batch;
-    private Texture img;
+    private Texture snakeHead;
+    private static final float MOVE_TIME = 1F;
+    private float timer = MOVE_TIME;
+    private static final int SNAKE_MOVEMENT = 32;
+    private static int snakeX = 0, snakeY = 0;
 
     @Override
     public void show() {
         batch = new SpriteBatch();
-        img = new Texture("badlogic.jpg");
+        snakeHead = new Texture(Gdx.files.internal("snakehead.png"));
     }
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(1, 0, 0, 1);
+        timer -= delta;
+        if (timer <= 0) {
+            timer = MOVE_TIME;
+            // System.out.println("Move");
+            snakeX += SNAKE_MOVEMENT;
+        }
+
+        // Set screen color to black
+        Gdx.gl.glClearColor(Color.BLACK.r, Color.BLACK.g, Color.BLACK.b, Color.BLACK.a);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
-        batch.draw(img, 0, 0);
+        // begin rendering code
+        batch.draw(snakeHead, snakeX, snakeY);
+        // en rendering code
         batch.end();
+
     }
 
 
