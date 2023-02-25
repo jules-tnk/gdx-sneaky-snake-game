cfeb5d39a048d1033442e8fb9cc533d513494fac
Kibalo Jules Tinaka <julestnk.dev@gmail.com>
2023-02-25 15:56:50 +0100

    Add apple Implement apple display logic Implement apple collision logic

--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -7,10 +7,14 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
 
 public class GameScreen extends ScreenAdapter {
     private SpriteBatch batch;
     private Texture snakeHead;
+    private Texture apple;
+    private boolean isAppleAvailable = false;
+    private static int appleX = 0, appleY = 0;
     private static final float MOVE_TIME = 1F;
     private float timer = MOVE_TIME;
     private static final int SNAKE_MOVEMENT = 32;
@@ -21,33 +25,48 @@ public class GameScreen extends ScreenAdapter {
     public void show() {
         batch = new SpriteBatch();
         snakeHead = new Texture(Gdx.files.internal("snakehead.png"));
+        apple = new Texture(Gdx.files.internal("apple.png"));
     }
 
     @Override
     public void render(float delta) {
         queryKeyboardInput();
-        timer -= delta;
+        timer -= delta*4;
         if (timer <= 0) {
             timer = MOVE_TIME;
             moveSnake();
             checkIfOutOfBounds();
         }
+        checkAppleCollision();
+        checkAndPlaceApple();
 
+        // set screen color
+        clearScreen();
+
+        // display sprites on screen
+        doBatchRender();
+    }
+
+    private void clearScreen() {
         // Set screen color to black
         Gdx.gl.glClearColor(Color.BLACK.r, Color.BLACK.g, Color.BLACK.b, Color.BLACK.a);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        doBatchRender();
-
     }
 
     private void doBatchRender() {
         batch.begin();
-        // begin rendering code
+
         batch.draw(snakeHead, snakeX, snakeY);
-        // en rendering code
+
+
+        if (isAppleAvailable) {
+            batch.draw(apple, appleX, appleY);
+        }
+
         batch.end();
     }
+
+
 
     private void checkIfOutOfBounds() {
         if (snakeX >= Gdx.graphics.getWidth()) {
@@ -100,6 +119,25 @@ public class GameScreen extends ScreenAdapter {
             snakeDirection = Direction.DOWN;
         }
 
+    }
+
+    private void checkAndPlaceApple() {
+        if (!isAppleAvailable) {
+            do {
+                appleX = MathUtils.random(0, (Gdx.graphics.getWidth() - SNAKE_MOVEMENT) / SNAKE_MOVEMENT) * SNAKE_MOVEMENT;
+                appleY = MathUtils.random(0, (Gdx.graphics.getHeight() - SNAKE_MOVEMENT) / SNAKE_MOVEMENT) * SNAKE_MOVEMENT;
+            } while (appleX == snakeX && appleY == snakeY);
+            isAppleAvailable = true;
+        }
+    }
+
+    private void checkAppleCollision() {
+        boolean condition1 = isAppleAvailable;
+        boolean condition2 = snakeX == appleX;
+        boolean condition3 = snakeY == appleY;
+        if (condition1 && condition2 && condition3) {
+            isAppleAvailable = false;
+        }
     }
 
 }
